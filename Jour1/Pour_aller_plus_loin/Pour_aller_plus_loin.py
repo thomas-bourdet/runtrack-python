c12bf5df3cc33dc5717385aa4e967a8e0c37da64
thomas-bourdet <thomas.bourdet@laplateforme.io>
2023-11-20 15:00:11 +0100

    Rendu Pour_aller_plus_loin

---
+++ Jour1/Pour_aller_plus_loin/Pour_aller_plus_loin.py
@@ -0,0 +1,6 @@
+var=input("Entrez une chaine de caractère: ")
+
+if 'e'in var :
+	print("La chaine de caractère contient la lettre E")
+else:
+	print("La chaine de caractère ne contient pas la lettre E")
