09df3eafea7c822c3dc29c4a2fe9af835f92a737
thomas-bourdet <thomas.bourdet@laplateforme.io>
2023-11-20 11:20:54 +0100

    First Push création des fichiers et Rendu des Jobs 1 2 et 3

---
+++ Jour1/Job01/job01.py
@@ -0,0 +1,11 @@
+print(10 + 3)
+
+print(10 * 3)
+
+print(10 % 3)
+
+print(10 - 3) 
+
+print(10 / 3) 
+
+print(10 // 3)
---
+++ Jour1/Job03/job03.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+
+#Défini le chemin du script actuel
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+#Défini le chemin des différents scripts
+script1_path = os.path.join(current_directory, "..", "Job01", "job01.py")
+script2_path = os.path.join(current_directory, "..", "Job02", "main.py")
+
+#Run job01.py et récupere le résultat
+result_job01 = subprocess.run(["python", script1_path], stdout=subprocess.PIPE, text=True)
+
+#Run main.py et récupere le résultat
+result_job02 = subprocess.run(["python", script2_path], stdout=subprocess.PIPE, text=True)
+
+
+print("Result from job01.py:")
+print(result_job01.stdout)
+
+print("\nResult from job02.py:")
+print(result_job02.stdout)
