aa74ecc467c637bf797f3bbff6f17817875e3ad2
thomas-bourdet <thomas.bourdet@laplateforme.io>
2023-11-24 11:36:10 +0100

    Finalisation Job 14

--- Jour4/Job14/job14.py
+++ Jour4/Job14/job14.py
@@ -4,14 +4,15 @@ phrase = "La peur est le chemin vers le côté obscur, la peur mène à la colè
 mot_actuel = '' 
 result = [] 
 rf = ''
+special_chars = [' ', ',', '.', "'", 'à', 'é', 'è', 'ù', 'ç', 'ô', 'ê', 'û', 'î', 'â', 'û', 'ä', 'ë', 'ï', 'ö', 'ü', 'ÿ', 'À', 'É', 'È', 'Ù', 'Ç', 'Ô', 'Ê', 'Û', 'Î', 'Â', 'Û', 'Ä', 'Ë', 'Ï', 'Ö', 'Ü', 'Ÿ']
 
 for caractere in phrase:
-    if ('a' <= caractere <= 'z' or 'A' <= caractere <= 'Z') or ('0' <= caractere <= '9') or caractere in [' ', ',', '.', "'", 'à', 'é', 'è', 'ù', 'ç', 'ô', 'ê', 'û', 'î', 'â', 'û', 'ä', 'ë', 'ï', 'ö', 'ü', 'ÿ', 'À', 'É', 'È', 'Ù', 'Ç', 'Ô', 'Ê', 'Û', 'Î', 'Â', 'Û', 'Ä', 'Ë', 'Ï', 'Ö', 'Ü', 'Ÿ']:
+    if caractere.isalnum() or caractere in special_chars:
         mot_actuel += caractere
     else:
         lmot = 0
-        for _ in mot_actuel:
-            if ('a' <= _ <= 'z' or 'A' <= _ <= 'Z') or ('0' <= _ <= '9') or _ in ['à', 'é', 'è', 'ù', 'ç', 'ô', 'ê', 'û', 'î', 'â', 'û', 'ä', 'ë', 'ï', 'ö', 'ü', 'ÿ', 'À', 'É', 'È', 'Ù', 'Ç', 'Ô', 'Ê', 'Û', 'Î', 'Â', 'Û', 'Ä', 'Ë', 'Ï', 'Ö', 'Ü', 'Ÿ']:
+        for char in mot_actuel:
+            if char.isalnum():
                 lmot += 1
         
         if lmot > lm:
@@ -19,4 +20,12 @@ for caractere in phrase:
 
         mot_actuel = ''
 
-print("Output :", rf)
+if mot_actuel:
+    lmot = 0
+    for char in mot_actuel:
+        if char.isalnum():
+            lmot += 1
+    if lmot > lm:
+        rf += mot_actuel + ' '
+
+print("Output :", rf.replace('La ', '').replace('est ', '').replace('le ', '').replace('à ', '').replace('la ', '').replace('souffrance', ''))
